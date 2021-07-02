989fad90c9191fdd9223ad3df485df797577eb45
Night Fury <83588906+NightFury31@users.noreply.github.com>
2021-07-02 16:23:41 +0530

    Add files via upload

---
+++ Day 14/app.js
@@ -0,0 +1,117 @@
+function online() {
+    alert("You're online.");
+}
+
+function offline() {
+    alert("Gone offline. Check internet connectivity.");
+}
+
+function onAbrt() {
+    alert("Failed to load. U pressed Esc");
+}
+
+function aftrLd() {
+    alert("U've printed data....");
+}
+
+function bfronprnt() {
+    alert("Msg before load...");
+}
+
+function bfruld() {
+    return "Dummy text........";
+}
+
+function blr() {
+    alert("Textbox lost focus......");
+}
+
+function ply() {
+    alert("Video can be played.......");
+}
+
+function plytrh() {
+    alert("Loaded without fail...... Wohoooo");
+}
+
+function chg() {
+    var l = document.getElementById("gender").value;
+    document.getElementById("abcc").innerHTML=l;
+
+}
+function chgclr()
+{
+    document.getElementById('abcc').style.backgroundColor = 'Red'; 
+}
+function ctxtmnu()
+{
+    alert("U've clicked on Context menu....");
+}
+function cpyy()
+{
+    alert("value copied.....")
+}
+function dbclickk()
+{
+    alert("U'be clicked here 2 times");
+}
+function drg()
+{
+    alert("U've tried to drag it....");
+}
+function drop()
+{
+    alert("On drop");
+}
+function rgovr()
+{
+    alert("On drag over");
+}
+function drgend()
+{
+    alert("Drag ended");
+}
+function drglv()
+{
+    alert("Drag leaved");
+}
+function drgstrt()
+{
+    alert("drag started");
+}
+function drgentr()
+{
+    alert("Drag enter");
+}
+function empty()
+{
+    alert("Empty element");
+}
+function prss()
+{
+    alert("Keypressed.....");
+}
+function msprs()
+{
+    alert("Mouse button pressed");
+}
+function tmupdt()
+{
+    alert("U've tried to update the timing of video....");
+}
+function plyd()
+{
+    alert("Started playing.....")
+}
+function rsz()
+{
+    alert("U tried to resize the window LoL");
+}
+function seekit()
+{
+    alert("Seek function called.....!!!");
+}
+function vlmchg()
+{
+    alert("On volume change...")
+}
