40e2b0e455f9298f5df464c80018cc79a615f43d
Night Fury <83588906+NightFury31@users.noreply.github.com>
2021-07-12 16:35:14 +0530

    Added DAy 16

---
+++ Day 16/Readme.md.txt
@@ -0,0 +1,1 @@
+https://csb-wmbci.vercel.app/
---
+++ Day 16/src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Chess  from  "./Component/Chess";
+import Meme  from  "./Component/Meme";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Meme />
+      <Chess />
+    </div>
+  );
+}
